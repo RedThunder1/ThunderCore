270b03fdc2be36fe14ac89ba8f273499d5119432
RedThunder <redthunderspartan@gmail.com>
2023-04-19 10:23:41 -0500

    Initial commit

---
+++ src/main/java/ThunderCore/ThunderCore.java
@@ -0,0 +1,22 @@
+package ThunderCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ThunderCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
